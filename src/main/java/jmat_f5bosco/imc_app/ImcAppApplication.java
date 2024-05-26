088afbaed8267a80165d3f7dce8cf0e07ca183d9
jmat732 <jmatisamuel@gmail.com>
2024-05-26 09:49:06 +0200

    add templates/html,static/css and images, java spring,and running firts time ok

---
+++ src/main/java/jmat_f5bosco/imc_app/ImcAppApplication.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.imc_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmat_f5bosco/imc_app/ImcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.imc_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
